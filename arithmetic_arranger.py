0afdbd975d501aa52cb4e2c1cc15efa042ebd0ce
Michael Green-Lab Computer <mjgreen2g@yahoo.com>
2021-11-04 19:01:17 -0500

    Added comments for clarity

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,38 +1,41 @@
-def arithmetic_arranger(problems, solve=None):
+#Solution to Arithmetic Arranger project
+#Created in Visual Studio 2019
+#by Michael Green
+
+def arithmetic_arranger(problems, solve = None):
 
     arranged_problems = ""
     listOfProblems = []
-
+                                                                #Check for proper number of problems (5 or less).
     if len(problems) > 5:
         return "Error: Too many problems."
 
     for problem in problems:
+                                                                #convert each problem string into list for analysis.
         converted_string_list = []
         dashline = ""
         converted_string_list = problem.split()
-
+                                                                #get largest item in problem. Must be less than 5 digits.
         largest_operand = len(max(converted_string_list, key = len))
         if largest_operand > 4:
             return "Error: Numbers cannot be more than four digits."
-
+                                                                #check sign for addtion or subtraction.
         sign = converted_string_list[1]
         if (sign != '+') and (sign != '-'):
             return "Error: Operator must be '+' or '-'."
-
+                                                                #operands must be digits.
         try: 
             operand1 = int(converted_string_list[0])
             operand2 = int(converted_string_list[2])
         except:
             return "Error: Numbers must only contain digits."
-
+                                                                #All error checks passed. Proceed.
+                                                                #Dashed line to seperate problem and solution. 
+                                                                #Length covers sign + space + largest operand.
+                                                                #Add to end of problem.
         dashline = ''.join(["-"] * (largest_operand + 2))
-
         converted_string_list.append(dashline)
-
-   #     arranged_problems += str(operand1).rjust(largest_operand + 2) + '\n' 
-   #     arranged_problems += sign + ' ' + (str(operand2)).rjust(largest_operand) + '\n'
-   #     arranged_problems += dashline.rjust(largest_operand + 2) + '\n'
-
+                                                                #If requested, calculate solution, add to end of problem
         if (solve == True):
             if (sign == "-"):
                 solution = (operand1 - operand2)
@@ -40,33 +43,30 @@ def arithmetic_arranger(problems, solve=None):
                 solution = (operand1 + operand2)
             converted_string_list.append(solution)   
 
-   #         arranged_problems += (str(solution).rjust(largest_operand + 2) + '\r')
         else:
             converted_string_list.append('')
-   #         arranged_problems += ('\r')
-
+                                                                #end of loop. Add problem to list. 
+                                                                #Cycle through loop until all problems processed
         listOfProblems.append(converted_string_list)
-
-    #operand1
+                                                                #Format for printing. Use dashed line to right justify
+                                                                #operand1 + 4 spaces + next operand1 + 4 spaces + ... newline
     arranged_problems += (listOfProblems[0][0]).rjust(len(listOfProblems[0][3]))
     for i in range (1,len(problems),1):
         arranged_problems += "    "
         arranged_problems += (listOfProblems[i][0]).rjust(len(listOfProblems[i][3]))
     arranged_problems += '\n'
-
-    #sign and operand2
+                                                                #sign + 1 space + operand2 + 4 spaces + next sign + ... new line
     arranged_problems += (listOfProblems[0][1]) + " " + (listOfProblems[0][2]).rjust(len(listOfProblems[0][3])-2)
     for i in range (1,len(problems),1):
         arranged_problems += "    "
         arranged_problems += (listOfProblems[i][1]) + " " + (listOfProblems[i][2]).rjust(len(listOfProblems[i][3])-2)
     arranged_problems += '\n'
-
-    #dashed line
+                                                                #dashed line + 4 spaces + next dashed line + ... 
     arranged_problems += (listOfProblems[0][3]).rjust(len(listOfProblems[0][3]))
     for i in range (1,len(problems),1):
         arranged_problems += "    "
         arranged_problems += (listOfProblems[i][3]).rjust(len(listOfProblems[i][3]))
-
+                                                                #If applicable: new line + solution + 4 spaces + ...
     if (solve == True):
         arranged_problems += '\n'
         arranged_problems += str(listOfProblems[0][4]).rjust(len(listOfProblems[0][3]))
